58ac4705cbd83d762b6c2c49b083fe6ff8ad8822
Andryxa0333 <andron_0333@mail.ru>
2025-03-11 20:33:44 +0300

    Some code optimisations

--- main.py
+++ main.py
@@ -61,14 +61,12 @@ def train_epoch(model, loader, optimizer, criterion, scheduler, device, epoch):
 
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-
         outputs = model(inputs)
 
         targets_expanded = targets.unsqueeze(1).expand(-1, outputs.size(1))
 
         outputs = outputs.view(-1, outputs.size(-1))  # (32 * 17, 17746)
         targets_expanded = targets_expanded.reshape(-1)
-
 
         loss = criterion(outputs, targets_expanded)
 
@@ -116,7 +114,7 @@ def evaluate(model, loader, criterion, device, epoch):
             loss = criterion(outputs, targets_expanded)
             total_loss += loss.item()
             _, predicted = torch.max(outputs, 1)
-            correct += (predicted == targets).sum().item()
+            correct += (predicted == targets_expanded).sum().item()/inputs.size(1)
             total += targets.size(0)
 
             writer.add_scalar("Val/Loss", loss.item(), epoch * len(loader) + batch_idx)
@@ -144,53 +142,30 @@ def log_final_plots(epochs, random_losses, curriculum_losses, random_accs, curri
     plt.close(fig)
 
 
-def train_random_sampling(model, train_loader, val_loader, optimizer, criterion, scheduler, device, epochs):
-    random_train_losses, random_val_losses = [], []
-    random_train_accs, random_val_accs = [], []
+# Случайное сэмплирование
+def train_learning(model, train_loader, val_loader, optimizer, criterion, scheduler, device, epochs):
+    train_losses, val_losses = [], []
+    train_accs, val_accs = [], []
 
     for epoch in range(epochs):
         print(f"\nRandom Sampling Epoch {epoch+1}/{epochs}")
         train_loss, train_acc = train_epoch(model, train_loader, optimizer, criterion, scheduler, device, epoch)
         val_loss, val_acc = evaluate(model, val_loader, criterion, device, epoch)
-        random_train_losses.append(train_loss)
-        random_train_accs.append(train_acc)
-        random_val_losses.append(val_loss)
-        random_val_accs.append(val_acc)
+        train_losses.append(train_loss)
+        train_accs.append(train_acc)
+        val_losses.append(val_loss)
+        val_accs.append(val_acc)
         print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%")
         print(f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.2f}%")
 
-    return random_train_losses, random_val_losses, random_train_accs, random_val_accs
+    return train_losses, val_losses, train_accs, val_accs
 
-def train_curriculum_learning(model, train_dataset, val_loader, optimizer, criterion, scheduler, device, epochs):
-    curriculum_train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False, collate_fn=collate_batch)
 
-    curriculum_train_losses, curriculum_val_losses = [], []
-    curriculum_train_accs, curriculum_val_accs = [], []
-
-    for epoch in range(epochs):
-        print(f"\nCurriculum Learning Epoch {epoch+1}/{epochs}")
-        train_loss, train_acc = train_epoch(model, curriculum_train_loader, optimizer, criterion, scheduler, device, epoch)
-        val_loss, val_acc = evaluate(model, val_loader, criterion, device, epoch)
-        curriculum_train_losses.append(train_loss)
-        curriculum_train_accs.append(train_acc)
-        curriculum_val_losses.append(val_loss)
-        curriculum_val_accs.append(val_acc)
-        print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%")
-        print(f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.2f}%")
-
-    return curriculum_train_losses, curriculum_val_losses, curriculum_train_accs, curriculum_val_accs
-
-log_dir = config.logging_dir
-writer = SummaryWriter(log_dir)
+writer = SummaryWriter(config.logging_dir)
 
 device = torch.device(config.training.device)
 
-train_split = config.data.train_split
-val_split = config.data.val_split
-random_seed = config.data.random_seed
-dataset_path = config.data.dataset_path
-
-df = pd.read_csv(dataset_path)
+df = pd.read_csv(config.data.dataset_path)
 
 def clean_expression(expression):
     expression = expression.replace(" ", "")
@@ -198,55 +173,31 @@ def clean_expression(expression):
 
 df['expression'] = df['expression'].apply(clean_expression)
 
-train_df, val_df = train_test_split(df, test_size=val_split, random_state=random_seed)
-
+train_df, val_df = train_test_split(df, test_size=config.data.train_split, random_state=config.data.random_seed)
 train_dataset = ExpressionDataset(train_df)
 val_dataset = ExpressionDataset(val_df)
+train_loader = DataLoader(train_dataset, batch_size=config.training.batch_size, shuffle=config.training.shuffle, collate_fn=collate_batch)
+val_loader = DataLoader(val_dataset, batch_size=config.training.batch_size, shuffle=False, collate_fn=collate_batch)
 
-batch_size = config.training.batch_size
-shuffle = config.training.shuffle
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_batch)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
 
-num_tokens = config.model.num_tokens
-embedding_dim = config.model.embedding_dim
-num_layers = config.model.num_layers
-num_heads = config.model.num_heads
-dropout = config.model.dropout
 num_classes = df['result'].max() - df['result'].min() + 1
 
 model_random = TransformerDecoder(
-    num_tokens=num_tokens,
-    n_embd=embedding_dim,
-    num_layers=num_layers,
-    num_heads=num_heads,
+    num_tokens=config.model.num_tokens,
+    n_embd=config.model.embedding_dim,
+    num_layers=config.model.num_layers,
+    num_heads=config.model.num_heads,
     num_classes=num_classes
 ).to(device)
 
-model_curriculum = TransformerDecoder(
-    num_tokens=num_tokens,
-    n_embd=embedding_dim,
-    num_layers=num_layers,
-    num_heads=num_heads,
-    num_classes=num_classes
-).to(device)
-
-learning_rate = config.training.learning_rate
-num_epochs = config.training.num_epochs
-
-optimizer_random = optim.Adam(model_random.parameters(), lr=learning_rate)
-scheduler_random = transformers.get_linear_schedule_with_warmup(optimizer_random, (num_epochs + 9) // 10, num_epochs)
+optimizer_random = optim.Adam(model_random.parameters(), lr=config.training.learning_rate)
+scheduler_random = transformers.get_linear_schedule_with_warmup(optimizer_random, (config.training.num_epochs + 9) // 10, config.training.num_epochs)
 criterion_random = nn.CrossEntropyLoss()
 
-optimizer_curriculum = optim.Adam(model_curriculum.parameters(), lr=learning_rate)
-scheduler_curriculum = transformers.get_linear_schedule_with_warmup(optimizer_curriculum, (num_epochs + 9) // 10, num_epochs)
-criterion_curriculum = nn.CrossEntropyLoss()
-
-epochs = num_epochs
-
-random_train_losses, random_val_losses, random_train_accs, random_val_accs = train_random_sampling(
-    model_random, train_loader, val_loader, optimizer_random, criterion_random, scheduler_random, device, epochs
+random_train_losses, random_val_losses, random_train_accs, random_val_accs = train_learning(
+    model_random, train_loader, val_loader, optimizer_random, criterion_random, scheduler_random, device, config.training.num_epochs
 )
+
 
 def operation_priority(expression):
     if '+' in expression or '-' in expression:
@@ -259,18 +210,30 @@ df['priority'] = df['expression'].apply(operation_priority)
 df = df.sort_values(by='priority')
 df = df.drop(columns=['priority'])
 
-train_df, val_df = train_test_split(df, test_size=val_split, random_state=random_seed)
+train_df, val_df = train_test_split(df, test_size=config.data.train_split, random_state=config.data.random_seed)
 train_dataset = ExpressionDataset(train_df)
 val_dataset = ExpressionDataset(val_df)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_batch)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
+train_loader = DataLoader(train_dataset, batch_size=config.training.batch_size, shuffle=False, collate_fn=collate_batch)
+val_loader = DataLoader(val_dataset, batch_size=config.training.batch_size, shuffle=False, collate_fn=collate_batch)
 
-curriculum_train_losses, curriculum_val_losses, curriculum_train_accs, curriculum_val_accs = train_curriculum_learning(
-    model_curriculum, train_dataset, val_loader, optimizer_curriculum, criterion_curriculum, scheduler_curriculum, device, epochs
+model_curriculum = TransformerDecoder(
+    num_tokens=config.model.num_tokens,
+    n_embd=config.model.embedding_dim,
+    num_layers=config.model.num_layers,
+    num_heads=config.model.num_heads,
+    num_classes=num_classes
+).to(device)
+
+optimizer_curriculum = optim.Adam(model_curriculum.parameters(), lr=config.training.learning_rate)
+scheduler_curriculum = transformers.get_linear_schedule_with_warmup(optimizer_curriculum, (config.training.num_epochs + 9) // 10, config.training.num_epochs)
+criterion_curriculum = nn.CrossEntropyLoss()
+
+curriculum_train_losses, curriculum_val_losses, curriculum_train_accs, curriculum_val_accs = train_learning(
+    model_curriculum, train_loader, val_loader, optimizer_curriculum, criterion_curriculum, scheduler_curriculum, device, config.training.num_epochs
 )
 
 log_final_plots(
-    epochs,
+    config.training.num_epochs,
     random_train_losses,
     curriculum_train_losses,
     random_train_accs,
@@ -281,20 +244,20 @@ writer.close()
 
 plt.figure(figsize=(12, 6))
 plt.subplot(121)
-plt.plot(range(1, epochs+1), random_train_losses, label='Random Train Loss')
-plt.plot(range(1, epochs+1), random_val_losses, label='Random Val Loss')
-plt.plot(range(1, epochs+1), curriculum_train_losses, label='Curriculum Train Loss')
-plt.plot(range(1, epochs+1), curriculum_val_losses, label='Curriculum Val Loss')
+plt.plot(range(1, config.training.num_epochs+1), random_train_losses, label='Random Train Loss')
+plt.plot(range(1, config.training.num_epochs+1), random_val_losses, label='Random Val Loss')
+plt.plot(range(1, config.training.num_epochs+1), curriculum_train_losses, label='Curriculum Train Loss')
+plt.plot(range(1, config.training.num_epochs+1), curriculum_val_losses, label='Curriculum Val Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.title('Loss Comparison')
 
 plt.subplot(122)
-plt.plot(range(1, epochs+1), random_train_accs, label='Random Train Accuracy')
-plt.plot(range(1, epochs+1), random_val_accs, label='Random Val Accuracy')
-plt.plot(range(1, epochs+1), curriculum_train_accs, label='Curriculum Train Accuracy')
-plt.plot(range(1, epochs+1), curriculum_val_accs, label='Curriculum Val Accuracy')
+plt.plot(range(1, config.training.num_epochs+1), random_train_accs, label='Random Train Accuracy')
+plt.plot(range(1, config.training.num_epochs+1), random_val_accs, label='Random Val Accuracy')
+plt.plot(range(1, config.training.num_epochs+1), curriculum_train_accs, label='Curriculum Train Accuracy')
+plt.plot(range(1, config.training.num_epochs+1), curriculum_val_accs, label='Curriculum Val Accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy (%)')
 plt.legend()
